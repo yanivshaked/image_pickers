1d550f3d915aa13d260169969648901e522c1ae2
Yaniv Shaked <shakedyaniv@gmail.com>
2020-08-11 21:37:01 +0300

    [iOS] Attempt to disable croping menu after image is selected

--- ios/Classes/ImagePickersPlugin.m
+++ ios/Classes/ImagePickersPlugin.m
@@ -123,42 +123,34 @@ static NSString *const CHANNEL_NAME = @"flutter/image_pickers";
                 NSLog(@"%@",image);
                 
                 if (image) {
+
+                    NSData *data2=UIImageJPEGRepresentation(image , 1.0);
+                    if (data2.length>compressSize) {
+                        //压缩
+                        data2=UIImageJPEGRepresentation(image, (float)(compressSize/data2.length));
+                    }
+                    NSLog(@"_____方法__%ld",data2.length);
+                    UIImage *image =[UIImage imageWithData:data2];
+                    //重命名并且保存
+                    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+                    formatter.dateFormat = @"yyyyMMddHHmmss";
+                    int  x = arc4random() % 10000;
                     
-                    BigImageViewController *big =[[BigImageViewController alloc]init];
-                    big.configuration =configuration ;
-                    big.image =image;
-                    big.doneEditImageBlock = ^(UIImage * imageE) {
-                        NSData *data2=UIImageJPEGRepresentation(imageE , 1.0);
-                        if (data2.length>compressSize) {
-                            //压缩
-                            data2=UIImageJPEGRepresentation(imageE, (float)(compressSize/data2.length));
-                        }
-                        NSLog(@"_____方法__%ld",data2.length);
-                        UIImage *image =[UIImage imageWithData:data2];
-                        //重命名并且保存
-                        NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
-                        formatter.dateFormat = @"yyyyMMddHHmmss";
-                        int  x = arc4random() % 10000;
-                        
-                        NSString *name = [NSString stringWithFormat:@"%@01%d",[formatter stringFromDate:[NSDate date]],x];
-                        NSString  *jpgPath = [NSHomeDirectory()     stringByAppendingPathComponent:[NSString stringWithFormat:@"Documents/%@",name]];
-                        
-                        //保存到沙盒
-                        [UIImageJPEGRepresentation(image,1.0) writeToFile:jpgPath atomically:YES];
-                        NSString *aPath3=[NSString stringWithFormat:@"%@/Documents/%@",NSHomeDirectory(),name];
-                        NSDictionary *photoDic =@{
-                            @"thumbPath":[NSString stringWithFormat:@"%@",aPath3],
-                            @"path":[NSString stringWithFormat:@"%@",aPath3],
-                        };
-                        //取出路径
-                        result(@[photoDic]);
-                        return ;
-                        
-                        
+                    NSString *name = [NSString stringWithFormat:@"%@01%d",[formatter stringFromDate:[NSDate date]],x];
+                    NSString  *jpgPath = [NSHomeDirectory()     stringByAppendingPathComponent:[NSString stringWithFormat:@"Documents/%@",name]];
+                    
+                    //保存到沙盒
+                    [UIImageJPEGRepresentation(image,1.0) writeToFile:jpgPath atomically:YES];
+                    NSString *aPath3=[NSString stringWithFormat:@"%@/Documents/%@",NSHomeDirectory(),name];
+                    NSDictionary *photoDic =@{
+                        @"thumbPath":[NSString stringWithFormat:@"%@",aPath3],
+                        @"path":[NSString stringWithFormat:@"%@",aPath3],
                     };
+                    //取出路径
+                    result(@[photoDic]);
+                    return ;
                     
-                    [[UIApplication sharedApplication].delegate.window.rootViewController presentViewController:big animated:YES completion:^{
-                    }];
+
                     
                 }else{
                     
